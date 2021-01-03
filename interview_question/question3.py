6f126957b9a84062fb5e373810e7f9cc37773576
FaziletKosure <47929186+FaziletKosure@users.noreply.github.com>
2021-01-03 15:28:06 +0100

    First solution

--- interview_question/question3.py
+++ interview_question/question3.py
@@ -1,1 +1,19 @@
+# Given a collection of distinct integers, return all possible permutations.
+
+# [1,2,3]
+
+# Output: [
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+# first Solution
+lst = [1, 2, 3]
+p = [[]]
+for i in range(len(lst)):
+    p = [[a] + b for a in lst for b in p if a not in b]
+print(p)
 
