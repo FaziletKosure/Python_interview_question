0bf08cbe45db45b264f4a16e99acc33530552df6
FaziletKosure <47929186+FaziletKosure@users.noreply.github.com>
2021-01-12 23:18:37 +0100

    first

--- interview_question/question4.py
+++ interview_question/question4.py
@@ -1,1 +1,19 @@
 
+def closing_in_sum(n):
+    x = [i for i in str(n)]  # [2, 5, 2, 0]
+    count = 0
+    for i in range(len(x)//2):
+        count += int(x[i]+x[-1-i])
+    if len(x) % 2:
+        count += int(x[len(x)//2])
+    return count
+
+
+closing_in_sum(251220)  # 72
+
+# The first and last digits are 2 and 0.
+# 2 and 0 form 20.
+# The second and second to last digits are 5 and 2.
+# 5 and 2 form 52.
+
+# 20 + 52 = 72
