755532d1b8f68bb5b1208634241bbb03ad461d1d
FaziletKosure <47929186+FaziletKosure@users.noreply.github.com>
2021-02-16 22:23:07 +0100

    Update question5.py

--- interview_question/question5.py
+++ interview_question/question5.py
@@ -1,1 +1,12 @@
+# Given n pairs of parentheses, write a code to generate all combinations of well-formed (valid for Python) parentheses.
+# For example, given n = 3, a solution set is:
+
+# [
+#   "((()))",
+#   "(()())",
+#   "(())()",
+#   "()(())",
+#   "()()()"
+# ]
+
 
